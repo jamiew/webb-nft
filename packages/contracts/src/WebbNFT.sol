e7626e91dad296e438589009cffcb728338aa5be
Jamie Dubs <jamie@jamiedubs.com>
2022-07-12 14:04:59 -0400

    Basic ERC1155 NFT + tests

---
+++ packages/contracts/src/WebbNFT.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {Owned} from "solmate/auth/Owned.sol";
+import {ERC1155} from "solmate/tokens/ERC1155.sol";
+import "forge-std/console2.sol";
+
+contract WebbNFT is Owned, ERC1155 {
+    string public baseURI;
+    uint256 public maxID;
+
+    error TokenDoesNotExist();
+
+    event BaseURIUpdated(string newBaseURI);
+    event MaxIDUpdated(uint256 newMaxID);
+
+    modifier tokenExists(uint256 id) {
+        if (id > maxID) {
+            revert TokenDoesNotExist();
+        }
+        _;
+    }
+
+    constructor(string memory _baseURI, uint256 _maxID) Owned(msg.sender) {
+        baseURI = _baseURI;
+        maxID = _maxID;
+    }
+
+    function setBaseURI(string memory _baseURI) public onlyOwner {
+        baseURI = _baseURI;
+        emit BaseURIUpdated(_baseURI);
+    }
+
+    function setMaxID(uint256 _maxID) public onlyOwner {
+        maxID = _maxID;
+        emit MaxIDUpdated(_maxID);
+    }
+
+    function mint(uint256 id) public tokenExists(id) {
+        console2.log("hello from mint() sender=%s id=%s", msg.sender, id);
+        _mint(msg.sender, id, 1, "");
+    }
+
+    function uri(uint256 id)
+        public
+        view
+        override
+        tokenExists(id)
+        returns (string memory)
+    {
+        return string.concat(baseURI, Strings.toString(id), ".jpg");
+    }
+}
+
+// ripped from OZ Strings
+library Strings {
+    function toString(uint256 value) internal pure returns (string memory) {
+        // Inspired by OraclizeAPI's implementation - MIT licence
+        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
+    }
+}
---
+++ packages/contracts/test/WebbNFT.t.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: CC0-1.0
+pragma solidity >=0.8.10 <0.9.0;
+
+import "forge-std/Test.sol";
+import "../src/WebbNFT.sol";
+
+contract WebbNFTTest is Test {
+    WebbNFT private nft;
+
+    address private owner =
+        vm.addr(uint256(keccak256(abi.encodePacked("owner"))));
+    address private minter =
+        vm.addr(uint256(keccak256(abi.encodePacked("minter"))));
+
+    function setUp() public {
+        nft = new WebbNFT("ipfs://lol", 5);
+        nft.setOwner(owner);
+        assertEq(nft.owner(), owner);
+        vm.deal(owner, 10 ether);
+        vm.deal(minter, 10 ether);
+    }
+
+    function testSetMaxID() public {
+        assertEq(nft.maxID(), 5); // default, from above
+
+        vm.prank(owner);
+        nft.setMaxID(666);
+        assertEq(nft.maxID(), 666);
+
+        vm.expectRevert("UNAUTHORIZED");
+        vm.prank(minter);
+        nft.setMaxID(42);
+        assertEq(nft.maxID(), 666); // old one
+    }
+
+    function testSetBaseURI() public {
+        assertEq(nft.baseURI(), "ipfs://lol"); // default, from above
+
+        vm.prank(owner);
+        nft.setBaseURI("ipfs://ok/");
+        assertEq(nft.baseURI(), "ipfs://ok/");
+
+        vm.expectRevert("UNAUTHORIZED");
+        vm.prank(minter);
+        nft.setBaseURI("ipfs://else/");
+        assertEq(nft.baseURI(), "ipfs://ok/"); // old one
+    }
+
+    function testMint() public {
+        assertEq(nft.balanceOf(minter, 1), 0);
+        vm.prank(minter);
+        nft.mint(1);
+        assertEq(nft.balanceOf(minter, 1), 1);
+
+        assertEq(nft.balanceOf(minter, 5), 0);
+        vm.prank(minter);
+        nft.mint(5);
+        assertEq(nft.balanceOf(minter, 5), 1);
+
+        vm.expectRevert(WebbNFT.TokenDoesNotExist.selector);
+        vm.prank(minter);
+        nft.mint(6);
+    }
+
+    function testUri() public {
+        vm.prank(owner);
+        nft.setBaseURI("ipfs://ok/");
+        console2.log("nft.uri(1)", nft.uri(1));
+        assertEq(nft.uri(1), "ipfs://ok/1.jpg");
+    }
+}

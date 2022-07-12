573e57fefa925fb59fd77934cd9d627253bdc6f0
Jamie Dubs <jamie@jamiedubs.com>
2022-07-12 12:41:43 -0400

    Removing more old code

--- packages/contracts/src/ExampleNFT.sol
+++ packages/contracts/src/ExampleNFT.sol
@@ -1,20 +1,10 @@
-// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 import {ERC721Base} from "./ERC721Base.sol";
 
-/// @author frolic.eth
-/// @title  Example NFT
-contract ExampleNFT is ERC721Base {
-    // ****************** //
-    // *** INITIALIZE *** //
-    // ****************** //
-
+contract WebbNFT is ERC721Base {
     constructor() ERC721Base("Example NFT", "EXAMPLE", 0.1 ether, 10_000) {}
-
-    // ************ //
-    // *** MINT *** //
-    // ************ //
 
     function mint(uint256 numToBeMinted)
         external
